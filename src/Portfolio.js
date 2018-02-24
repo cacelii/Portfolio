a67d6e0ca060dc1cdeaefe7d4703826553ff9355
Cecilia Chang <ceciliafchang@gmail.com>
2018-02-23 23:25:02 -0500

    update skills, portfolio, resume

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -3,6 +3,7 @@ import globalKitchen from './images/global-kitchen.png';
 import HungerMatch from './images/HungerMatch.png';
 import CageMatch from './images/CageMatch.png';
 import BlogPost from './images/BlogPost.png';
+import GuessingGame from './images/GuessingGame.png';
 
 export default function Portfolio() {
   return (
@@ -20,11 +21,21 @@ export default function Portfolio() {
             src={globalKitchen}
             alt="global-kitchen"
           />
-          <button>
-            <a href="http://global-kitchen.herokuapp.com/" target="_blank">
-              Demo
-            </a>
-          </button>
+          <div className="buttons">
+            <button>
+              <a href="http://global-kitchen.herokuapp.com/" target="_blank">
+                Demo
+              </a>
+            </button>
+            <button>
+              <a
+                href="https://github.com/GH-FoodNetwork/Global-Kitchen"
+                target="_blank"
+              >
+                Code
+              </a>
+            </button>
+          </div>
         </li>
         <li>
           <h3>HungerMatch</h3>
@@ -38,14 +49,29 @@ export default function Portfolio() {
             src={HungerMatch}
             alt="HungerMatch"
           />
-          <button>
-            <a
-              href="https://www.youtube.com/watch?v=1maNAy3WxgI"
-              target="_blank"
-            >
-              Demo
-            </a>
-          </button>
+          <div className="buttons">
+            <button>
+              <a
+                href="https://www.youtube.com/watch?v=1maNAy3WxgI"
+                target="_blank"
+              >
+                Demo
+              </a>
+            </button>
+            <button>
+              <a href="https://github.com/cacelii/HungerMatch" target="_blank">
+                Mobile
+              </a>
+            </button>
+            <button>
+              <a
+                href="https://github.com/cacelii/HungerMatch-Web"
+                target="_blank"
+              >
+                Web
+              </a>
+            </button>
+          </div>
         </li>
         <li>
           <h3>Cage Match</h3>
@@ -55,11 +81,21 @@ export default function Portfolio() {
             Sequelize, Mocha, Chai, and Sass.
           </p>
           <img className="portfolio-image" src={CageMatch} alt="cage-match" />
-          <button>
-            <a href="http://cage-match.herokuapp.com/" target="_blank">
-              Demo
-            </a>
-          </button>
+          <div className="buttons">
+            <button>
+              <a href="http://cage-match.herokuapp.com/" target="_blank">
+                Demo
+              </a>
+            </button>
+            <button>
+              <a
+                href="https://github.com/DatGreekChick/glamazonians"
+                target="_blank"
+              >
+                Code
+              </a>
+            </button>
+          </div>
         </li>
         <li>
           <h3>React Native vs. Plain Old React</h3>
@@ -68,14 +104,46 @@ export default function Portfolio() {
             React, utilizing examples from my Stackathon project, HungerMatch.
           </p>
           <img className="portfolio-image" src={BlogPost} alt="blog-post" />
-          <button>
-            <a
-              href="https://medium.com/@ceciliafchang/react-native-vs-plain-old-react-bb0f27677cfd"
-              target="_blank"
-            >
-              Read
-            </a>
-          </button>
+          <div className="buttons">
+            <button>
+              <a
+                href="https://medium.com/@ceciliafchang/react-native-vs-plain-old-react-bb0f27677cfd"
+                target="_blank"
+              >
+                Read
+              </a>
+            </button>
+          </div>
+        </li>
+        <li>
+          <h3>Guessing Game</h3>
+          <p className="description">
+            Guess a number between 1-100. You have 5 attempts and you get hints
+            along the way. Built with HTML, CSS, Bootstrap, and Javascript.
+          </p>
+          <img
+            className="portfolio-image"
+            src={GuessingGame}
+            alt="guessing-game"
+          />
+          <div className="buttons">
+            <button>
+              <a
+                href="https://cacelii.github.io/Guessing-Game/"
+                target="_blank"
+              >
+                Demo
+              </a>
+            </button>
+            <button>
+              <a
+                href="https://github.com/cacelii/Guessing-Game"
+                target="_blank"
+              >
+                Code
+              </a>
+            </button>
+          </div>
         </li>
       </ul>
     </div>
--- src/Skills.js
+++ src/Skills.js
@@ -9,15 +9,17 @@ export default function Skills() {
         <li>React</li>
         <li>Redux</li>
         <li>React-Redux</li>
-        <li>Express</li>
+        <li>Express.js</li>
         <li>Node.js</li>
         <li>React Native</li>
+        <li>PostgreSQL</li>
         <li>Sequelize</li>
         <li>Git</li>
         <li>ES6</li>
         <li>HTML5</li>
         <li>CSS3</li>
         <li>Agile</li>
+        <li>REST</li>
       </ul>
       <h1>Knowledgeable</h1>
       <ul className="list">
@@ -28,15 +30,17 @@ export default function Skills() {
         <li>Jasmine</li>
         <li>Socket.io</li>
         <li>OAuth</li>
-        <li>Expo</li>
+        <li>APIs</li>
+        <li>SQL</li>
+        <li>Webpack</li>
       </ul>
       <h1>Some Familiarity</h1>
       <ul className="list">
-        <li>SQL</li>
         <li>jQuery</li>
         <li>Bootstrap</li>
         <li>Heroku</li>
-        <li>Sass</li>
+        <li>SCSS</li>
+        <li>A-Frame</li>
       </ul>
     </div>
   );
